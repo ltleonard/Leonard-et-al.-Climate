4be6d16f00e046c642da034692c11b16c67a6f21
ltleonard <42048155+ltleonard@users.noreply.github.com>
2021-03-18 11:12:37 -0600

    Update and rename Paper2_CCA.R to CCA_OrdinationMatrices.R

--- CCA_OrdinationMatrices.R
+++ CCA_OrdinationMatrices.R
@@ -7,8 +7,8 @@ q=as.matrix(ufrac)
 d<-data.frame(sample_data(CB16_LM)) 
 adonis<-adonis(q~Year, data=d,permutations=999)
 adonis
-#_______________Middle 16S_______________#
 write.csv(q, "LM.csv")
+#_______________Middle 16S_______________#
 #ADONIS test 
 groupsig = transform_sample_counts(CB16_LS_regular  , function(x) 100* x/ sum(x))
 ufrac<-UniFrac(groupsig, weighted=TRUE, normalized=TRUE, parallel=FALSE, fast=TRUE)
@@ -17,8 +17,6 @@ d<-data.frame(sample_data(CB16_LS_regular))
 adonis<-adonis(q~Date, data=d,permutations=999)
 adonis
 write.csv(q, "LS.csv")
-#########################################################################################################
-#Let's do 18S as well
 #_______________Middle-ES 16S_______________#
 #ADONIS test
 groupsig = transform_sample_counts(CB16_LS_induced  , function(x) 100* x/ sum(x))
@@ -28,6 +26,9 @@ d<-data.frame(sample_data(CB16_LS_induced))
 adonis<-adonis(q~Date, data=d,permutations=999)
 adonis
 write.csv(q, "ES.csv")
+#########################################################################################################
+#### Let's do 18S as well ####
+#########################################################################################################
 #_______________Lower 18S_______________#
 #ADONIS test 
 groupsig = transform_sample_counts(CB18_LM  , function(x) 100* x/ sum(x))
@@ -36,6 +37,7 @@ q=as.matrix(ufrac)
 d<-data.frame(sample_data(CB18_LM)) 
 adonis<-adonis(q~Year, data=d,permutations=999)
 adonis
+write.csv(q, "LM18.csv")
 #_______________Middle 18S_______________#
 write.csv(q, "LM.csv")
 #ADONIS test 
@@ -45,7 +47,7 @@ q=as.matrix(ufrac)
 d<-data.frame(sample_data(CB18_LS_regular)) 
 adonis<-adonis(q~Date, data=d,permutations=999)
 adonis
-write.csv(q, "LS.csv")
+write.csv(q, "LS18.csv")
 #_______________Middle-ES 18S_______________#
 #ADONIS test
 groupsig = transform_sample_counts(CB18_LS_induced  , function(x) 100* x/ sum(x))
@@ -54,4 +56,4 @@ q=as.matrix(ufrac)
 d<-data.frame(sample_data(CB18_LS_induced)) 
 adonis<-adonis(q~Date, data=d,permutations=999)
 adonis
-write.csv(q, "ES.csv")
+write.csv(q, "SM18.csv")
